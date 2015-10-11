3d415fa1689246a1350d2e935c8e1c50ae6cb80b
James Henstridge <james@jamesh.id.au>
2015-10-11 22:33:27 +0800

    Make example program catch exceptions.  Fix snapcraft.yaml.

--- mycat.py
+++ mycat.py
@@ -3,11 +3,15 @@
 import sys
 
 def main(argv):
-    with open(argv[1], 'r') as fp:
-        buf = fp.read(4096)
-        while buf:
-            sys.stdout.write(buf)
+    try:
+        with open(argv[1], 'r') as fp:
             buf = fp.read(4096)
+            while buf:
+                sys.stdout.write(buf)
+                buf = fp.read(4096)
+    except Exception as exc:
+        print("Error:", str(exc))
+        return 1
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
